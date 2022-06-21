1c140f512a00fb3d8c3e4f3d6dd4888e6ec134a0
william-rde <william.rodde4@gmail.com>
2022-06-21 16:10:44 +0200

    commentaire

--- interactive-story/src/App.js
+++ interactive-story/src/App.js
@@ -1,15 +1,19 @@
 import logo from './logo.svg';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./components/SignIn";
+import Sondage from "./components/sondage";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SignIn />} />
+        <Route path="/sondage" element={<Sondage />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
