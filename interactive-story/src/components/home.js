f11a55a8e7e0077536969e5e1fa8392748acdd21
R3m1r0 <remicanta65@gmail.com>
2022-06-23 10:25:48 +0200

    ok

--- interactive-story/src/components/home.js
+++ interactive-story/src/components/home.js
@@ -39,7 +39,7 @@ const Home = () => {
                     </div>
                     <div className="contenu">
                       <td>Au pied des Pyrénées, entre Pau et Lourdes, Christian Delacoux sculpte  son univers depuis plus de vingt ans.
-Il vous présente ici un aperçu de ses œuvres passées ainsi que toutes ses sculptures disponibles à la vente et rassemblées dans sa salle d'exposition personnelle à Montaut (64800).</td>
+Il vous présente ici un aperçu de ses œuvres passées ainsi que toutes ses sculptures disponibles à la vente et rassemblées dans sa salle d'exposition personnelle à Montaut (64800). <b>Rue Torte, 64800 Montaut</b></td>
                     </div>
                   </tr>
                 </div>
@@ -55,7 +55,7 @@ Il vous présente ici un aperçu de ses œuvres passées ainsi que toutes ses sc
                       </div>
                     </div>
                     <div className="contenu">
-                      <td>Spectacles humoristiques : One man show pour soirée théâtre et public ado/adultes + duo burlesque pour tous (de 5 à 105 ans, francophones ou non) sur le thème des cow-boys.</td>
+                      <td>Spectacles humoristiques : One man show pour soirée théâtre et public ado/adultes + duo burlesque pour tous (de 5 à 105 ans, francophones ou non) sur le thème des cow-boys. <b>Boulevard Jean Moulin, 65000 Tarbes</b> </td>
                     </div>
                   </tr>
                 </div>
@@ -71,7 +71,8 @@ Il vous présente ici un aperçu de ses œuvres passées ainsi que toutes ses sc
                     </div>
                     <div className="contenu">
                       <td>Artiste tarbais, il est multi-instrumentiste, joue et compose une musique qui est un mix de blues et de sonorités indiennes. Parcours atypique car il a du surmonter son handicap (malentendant), est diplômé de la MAI de Nancy puis de la JAI de Nancy, a rencontré les plus grands musiciens de blues à  Boston.
-En 2014, il découvre à Vancouver son instrument de prédilection le Mohan Veena, instrument à 22 cordes.</td>
+En 2014, il découvre à Vancouver son instrument de prédilection le Mohan Veena, instrument à 22 cordes. <b>Rue Dauriac, 65000 Tarbes</b>
+</td>
                     </div>
                   </tr>
                 </div>
